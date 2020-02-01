08ca8adefacf43e2079462135a4c4c42b628c1f1
Aleksandra Simeonova <alekssandrans@gmail.com>
2020-02-01 02:53:12 +0200

    Responsive slideshow added.

--- js/script.js
+++ js/script.js
@@ -6,5 +6,24 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
     });
 }
+const changeImage1 = () => {
+    document.getElementById('home-image').src ="C:\\Users\\rv\\Desktop\\MyRestaurant\\img\\home-1.jpg";
+    document.getElementById('food').classList.add('active-link-text');
+    document.getElementById('wine').classList.remove('active-link-text');
+    document.getElementById('view').classList.remove('active-link-text');
+
+}
+const changeImage2 = () => {
+    document.getElementById('home-image').src ="C:\\Users\\rv\\Desktop\\MyRestaurant\\img\\home-2.jpg";
+    document.getElementById('wine').classList.add('active-link-text');
+    document.getElementById('food').classList.remove('active-link-text');
+    document.getElementById('view').classList.remove('active-link-text');
+}
+const changeImage3 = () => {
+    document.getElementById('home-image').src ="C:\\Users\\rv\\Desktop\\MyRestaurant\\img\\home-3.jpg";
+    document.getElementById('view').classList.add('active-link-text');
+    document.getElementById('wine').classList.remove('active-link-text');
+    document.getElementById('food').classList.remove('active-link-text');
+}
 
 navSlide();
